57544ed2dabfca86d6e0095d3e36118448f88536
Mikko Tomperi <mikko.tomperi@cybercom.com>
2015-08-13 09:46:53 +0300

    removed obsolete scripts, reconfigured assembly files

--- restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-basepath=$1
-cd $basepath
-export CATALINA_BASE=/data00/services/kapa-rova-api
-export CATALINA_HOME=/devtools/apache-tomcat-8.0.18
-/devtools/apache-tomcat-8.0.18/bin/catalina.sh stop
-/devtools/apache-tomcat-8.0.18/bin/catalina.sh start
