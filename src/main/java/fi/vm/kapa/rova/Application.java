3237552b702baf3de5570a6d4e3553492e49db0f
jkorkalainen <juha.korkalainen@iki.fi>
2015-09-18 11:25:36 +0300

    disable dispatcher servlet

--- src/main/java/fi/vm/kapa/rova/Application.java
+++ src/main/java/fi/vm/kapa/rova/Application.java
@@ -1,12 +1,18 @@
 package fi.vm.kapa.rova;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={WebMvcAutoConfiguration.class, DispatcherServletAutoConfiguration.class})
+
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
