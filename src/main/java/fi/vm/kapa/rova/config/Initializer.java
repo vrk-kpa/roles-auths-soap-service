c85bf1e8604bf5b507c29ed9952aba5807c7bd73
Jari Saukkonen <jari.saukkonen@ambientia.fi>
2016-04-29 15:30:56 +0300

    A slight kludge to make roles-auth-soap-service exit cleanly when ctrl-c is pressed

--- src/main/java/fi/vm/kapa/rova/config/Initializer.java
+++ src/main/java/fi/vm/kapa/rova/config/Initializer.java
@@ -1,19 +1,24 @@
 package fi.vm.kapa.rova.config;
 
-import com.sun.xml.ws.transport.http.servlet.WSSpringServlet;
-import fi.vm.kapa.rova.logging.LogbackConfigurator;
-import fi.vm.kapa.rova.logging.MDCFilter;
+import java.lang.reflect.Field;
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-import javax.servlet.DispatcherType;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import java.util.EnumSet;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import com.sun.xml.ws.transport.http.servlet.WSServletDelegate;
+import com.sun.xml.ws.transport.http.servlet.WSSpringServlet;
+
+import fi.vm.kapa.rova.logging.LogbackConfigurator;
+import fi.vm.kapa.rova.logging.MDCFilter;
 
 @Configuration
 @ImportResource("applicationContext.xml")
@@ -33,14 +38,31 @@ public class Initializer extends SpringBootServletInitializer {
     public void onStartup(ServletContext ctx) throws ServletException {
         super.onStartup(ctx);
         ctx.addFilter(MDC_FILTER, MDCFilter.class)
-                .addMappingForUrlPatterns(
-                        EnumSet.of(DispatcherType.REQUEST,
-                                DispatcherType.FORWARD), false, "/*");
+                .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD), false, "/*");
     }
-
 
     @Bean
     public WSSpringServlet wsSpringServlet() {
-        return new WSSpringServlet();
+        return new WSSpringServlet() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void destroy() {
+                // Problem: WSSpringServlet tries to destroy the WebApplicationContext on its own,
+                // in addition to the usual shutdown from ContextLoaderListener. This causes a deadlock
+                // preventing the JVM from shutting down.
+                //
+                // Solution: A custom destroy()-implementation that doesn't try to destroy the context. Unfortunately
+                // the internal 'delegate' reference is private, so we need some reflection wizardry to access it. 
+                try {
+                    Field f = WSSpringServlet.class.getDeclaredField("delegate");
+                    f.setAccessible(true);
+                    WSServletDelegate delegate = (WSServletDelegate)f.get(this);
+                    delegate.destroy();
+                } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
     }
 }
