549cf34cdac597b6bf0e81564b0fb03a9f74dbf6
Jari Saukkonen <jari.saukkonen@ambientia.fi>
2016-06-13 11:59:25 +0300

    Fix typo in exception handling, request id was accidentally lost

--- src/main/java/fi/vm/kapa/rova/soap/providers/EngineDataProvider.java
+++ src/main/java/fi/vm/kapa/rova/soap/providers/EngineDataProvider.java
@@ -244,7 +244,7 @@ public class EngineDataProvider implements DataProvider, SpringProperties {
         Map<String, String> attributes = getAttributes(response);
         
         return String.format("RequestId: %s, Date: %s, Status: %d %s, Message: %s",
-                valueOrDefault(attributes.get("ReqID"), "NO_SESSION"),
+                valueOrDefault(attributes.get(Logger.REQUEST_ID), "NO_SESSION"),
                 response.getDate().toString(),
                 response.getStatusInfo().getStatusCode(),
                 response.getStatusInfo().getReasonPhrase(),
