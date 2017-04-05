c8382339fd02e1253a7f4d87a17fedbba57b5311
jteuho <juho.teuho@gofore.com>
2017-04-05 12:16:53 +0300

    eureka-conffeja

--- src/main/java/fi/vm/kapa/rova/Application.java
+++ src/main/java/fi/vm/kapa/rova/Application.java
@@ -28,12 +28,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={WebMvcAutoConfiguration.class, DispatcherServletAutoConfiguration.class})
-
+@EnableDiscoveryClient
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
